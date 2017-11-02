fce349f6989d3f45b555d9d2d8edf8387951c9a3
woodcrust <roboucrop@gmail.com>
2017-11-02 05:08:43 +0200

    fix the overriding of hash attributes

--- lib/flexserializer/base.rb
+++ lib/flexserializer/base.rb
@@ -3,12 +3,15 @@ module Flexserializer
     class << self
       attr_accessor :groups, :data_default_attributes
 
+      def inherited(base)
+        base.groups = {}
+      end
+
       def default_attributes(&block)
         self.data_default_attributes = block
       end
 
       def group(*group_names, &block)
-        self.groups ||= {}
         group_names.each do |name_group|
           self.groups[name_group] ||= []
           self.groups[name_group] << block
@@ -16,32 +19,53 @@ module Flexserializer
       end
     end
 
-    attr_reader :group_name
-    
+    attr_reader :group_name, :_attributes_data, :define_options
+
     def initialize(object, options = {})
-      @group_name = options[:group]
-      define_attributes
       super(object, options)
+      @_attributes_data = {}
+      @group_name       = options[:group]
+      @define_options   = options.clone
+      make_all_attributes
     end
 
-    def define_attributes
-      clear_data
+    def define_attributes(*attrs)
+      attrs = attrs.first if attrs.first.class == Array
+      attrs.each do |attr|
+        define_attribute(attr)
+      end
+    end
+
+    def define_attribute(attr, options = {}, &block)
+      key = options.fetch(:key, attr)
+      _attributes_data[key] = Attribute.new(attr, options, block)
+    end
+
+    def make_all_attributes
       define_default_attrs
       define_group_attrs
     end
 
-    def clear_data
-      self.class._attributes_data = {}
-    end
-
     def define_default_attrs
       return unless self.class.data_default_attributes
-      self.class.data_default_attributes.call
+      self.instance_eval &self.class.data_default_attributes
     end
 
     def define_group_attrs
-      return if !self.class.groups or !self.class.groups.keys.include?(group_name)
-      self.class.groups[group_name].each { |block| block.call }
+      self.class.groups.send(:[], group_name)&.each do |block| 
+        self.instance_eval(&block)
+      end
+    end
+
+    #override serializer methods
+
+    def attributes(requested_attrs = nil, reload = false)
+      @attributes = nil if reload
+      @attributes ||= _attributes_data.each_with_object({}) do |(key, attr), hash|
+        next if attr.excluded?(self)
+        next unless requested_attrs.nil? || requested_attrs.include?(key)
+        hash[key] = attr.value(self)
+      end
     end
   end
 end
--- lib/flexserializer/version.rb
+++ lib/flexserializer/version.rb
@@ -1,3 +1,3 @@
 module Flexserializer
-  VERSION = "1.2.4"
+  VERSION = "1.3.0"
 end
